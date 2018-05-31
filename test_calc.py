87e0f91a0ab4f141f097d7815ca0f310a5d355a6
Jonathan Dayton <jonathandayton23@gmail.com>
2018-05-31 15:53:39 -0600

    Add directory and test structure

---
+++ test_calc.py
@@ -0,0 +1,9 @@
+import unittest
+from calc import string
+
+class TestEmptyStringReturnsZero(unittest.TestCase):
+    def test_empty_returns_zero(self):
+        self.assertEqual(1, 2)
+
+if __name__ == "__main__":
+    unittest.main()
